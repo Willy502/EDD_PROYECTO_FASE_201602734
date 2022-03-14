fa924608370fde9409a3412f7a7a2c9b7d6ab4b0
Alejandro Barrios <w.alejandro.barrios@gmail.com>
2022-03-13 18:33:12 -0600

    Base structure

--- Fase2/src/main/java/barrios/alejandro/udrawingpage/MainApplication.java
+++ Fase2/src/main/java/barrios/alejandro/udrawingpage/MainApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Iniciar sesión");
         stage.setScene(scene);
         stage.show();
     }
---
+++ Fase2/src/main/java/barrios/alejandro/udrawingpage/users/model/User.java
@@ -0,0 +1,10 @@
+package barrios.alejandro.udrawingpage.users.model;
+
+public class User {
+
+    private int id;
+    private String name;
+    private String dpi;
+    private String password;
+
+}
