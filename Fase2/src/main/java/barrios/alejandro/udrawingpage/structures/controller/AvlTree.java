b0cf0b23fb83bd16ef105955d58b5e5526e27713
Alejandro Barrios <w.alejandro.barrios@gmail.com>
2022-03-19 15:53:28 -0600

    AVL Tree

--- Fase2/src/main/java/barrios/alejandro/udrawingpage/structures/controller/AvlTree.java
+++ Fase2/src/main/java/barrios/alejandro/udrawingpage/structures/controller/AvlTree.java
@@ -9,10 +9,11 @@ public class AvlTree {
             root = new Node(image);
             return;
         }
-
+        binaryInsert(root, image);
     }
 
     private void binaryInsert(Node current, BinarySearchTree image) {
+
         if (image.id >= current.image.id) {
             // right
             if (current.rightBranch == null) {
@@ -30,10 +31,69 @@ public class AvlTree {
                 binaryInsert(current, image);
             }
         }
+
+        current.height = 1 + maxHeight(height(current.leftBranch), height(current.rightBranch));
+
+        int fe = getFE(current); // Factor de equilibrio
+
+        // right rotate
+        if (fe > 1 && image.id < current.leftBranch.image.id) root = rightRotate(current);
+
+        // Left rotate
+        if (fe < -1 && image.id > current.rightBranch.image.id) root = leftRotate(current);
+
+        // Double right rotate
+        if (fe > 1 && image.id > current.leftBranch.image.id) {
+            current.leftBranch = leftRotate(current.leftBranch);
+            root = rightRotate(current);
+        }
+
+        // Double left rotate
+        if (fe < -1 && image.id < current.rightBranch.image.id) {
+            current.rightBranch = rightRotate(current.rightBranch);
+            root = leftRotate(current);
+        }
     }
 
-    private void balanceNode(Node current) {
+    private int getFE(Node current) {
+        if (current == null) return 0;
 
+        return height(current.leftBranch) - height(current.rightBranch);
+    }
+
+    private int height(Node current) {
+        if (current == null) return 0;
+        return current.height;
+    }
+
+    private int maxHeight(int a, int b) {
+        return Math.max(a, b);
+    }
+
+    private Node rightRotate(Node current) {
+        Node newRoot = current.leftBranch;
+        Node temp = newRoot.rightBranch;
+
+        newRoot.rightBranch = current;
+        current.leftBranch = temp;
+
+        current.height = maxHeight(height(current.leftBranch), height(current.rightBranch)) + 1;
+        newRoot.height = maxHeight(height(newRoot.leftBranch), height(newRoot.rightBranch)) + 1;
+
+        return newRoot;
+    }
+
+    private Node leftRotate(Node current) {
+        Node newRoot = current.rightBranch;
+        Node temp = newRoot.leftBranch;
+
+        newRoot.leftBranch = current;
+        current.rightBranch = temp;
+
+        current.height = maxHeight(height(current.leftBranch), height(current.rightBranch)) + 1;
+        newRoot.height = maxHeight(height(newRoot.leftBranch), height(newRoot.rightBranch)) + 1;
+
+        return newRoot;
     }
 
     static class Node {
@@ -43,6 +103,7 @@ public class AvlTree {
 
         public Node(BinarySearchTree image) {
             this.image = image;
+            height = 1;
         }
     }
 
